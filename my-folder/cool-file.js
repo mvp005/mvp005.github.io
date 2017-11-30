4b4fbea0c06629cbb91aa38df405db2ec5949765
Glitch (aframe) <none>
2017-11-30 03:06:35 +0000

    💶👠 Checkpoint my-folder/cool-file.js:554823

--- my-folder/cool-file.js
+++ my-folder/cool-file.js
@@ -2,7 +2,7 @@ function loadWalking() {
   let human = document.getElementById("movement");
   let html = '';
   const SCALE = 500;
-  const DIST = 0.2;
+  var DIST = 0.3;
   for (var i = 0; i < 20; i += 1) {
     html += `<a-animation easing="ease-in-sine" attribute="position" dur="` + SCALE.toString() + `" from= ".05 1 ` + (-i*DIST*4).toString() + `" to="0 1.1 ` + (-i*4*DIST - DIST).toString() + `" begin = "` +  (3000 + i * 4 * SCALE).toString() + `"></a-animation>
     <a-animation easing="ease-out-sine" attribute="position" dur="` + SCALE.toString() + `" from= "0.0 1.1 ` + (-i*DIST*4 - DIST).toString() + `" to= "-.05 1 ` + (-i*4*DIST - DIST * 2).toString() + `" begin = "` +  (3000 + i * 4 * SCALE + SCALE).toString() + `"></a-animation>
@@ -10,7 +10,13 @@ function loadWalking() {
     <a-animation easing="ease-out-sine" attribute="position" dur="` + SCALE.toString() + `" from= "0 1.1 ` + (-i*DIST*4 - DIST * 3).toString() + `" to= ".05 1 ` + (-i*4*DIST - DIST * 4).toString() + `" begin = "` +  (3000 + i * 4 * SCALE + SCALE * 3).toString() + `"></a-animation>`
   }
   
-  ht<a-animation easing="ease-out-sine" attribute="position" dur="500" from="0 1.1 -15.8" to=".05 1 -16" begin="42500"></a-animation>
+  DIST = 0.15;
+  for (var i = 20; i < 40; i += 1) {
+    html += `<a-animation easing="ease-in-sine" attribute="position" dur="` + SCALE.toString() + `" from= ".05 1 ` + (-i*DIST*4).toString() + `" to="0 1.1 ` + (-i*4*DIST - DIST).toString() + `" begin = "` +  (10000 + i * 4 * SCALE).toString() + `"></a-animation>
+    <a-animation easing="ease-out-sine" attribute="position" dur="` + SCALE.toString() + `" from= "0.0 1.1 ` + (-i*DIST*4 - DIST).toString() + `" to= "-.05 1 ` + (-i*4*DIST - DIST * 2).toString() + `" begin = "` +  (10000 + i * 4 * SCALE + SCALE).toString() + `"></a-animation>
+    <a-animation easing="ease-in-sine" attribute="position" dur="` + SCALE.toString() + `" from= "-.05 1 ` + (-i*DIST*4 - DIST * 2).toString() + `" to="0 1.1 ` + (-i*4*DIST - DIST * 3).toString() + `" begin = "` +  (10000 + i * 4 * SCALE + SCALE * 2).toString() + `"></a-animation>
+    <a-animation easing="ease-out-sine" attribute="position" dur="` + SCALE.toString() + `" from= "0 1.1 ` + (-i*DIST*4 - DIST * 3).toString() + `" to= ".05 1 ` + (-i*4*DIST - DIST * 4).toString() + `" begin = "` +  (10000 + i * 4 * SCALE + SCALE * 3).toString() + `"></a-animation>` 
+  }
 
   human.innerHTML = html;
 }
